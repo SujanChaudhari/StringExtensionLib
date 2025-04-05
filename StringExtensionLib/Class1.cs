41949b09f3dd341cf621a0730833d7dec3486180
SujanChaudhari <schaud91@my.centennialcollege.ca>
2025-04-05 11:25:45 -0400

    Initial Commit - Class Library

---
+++ StringExtensionLib/Class1.cs
@@ -0,0 +1,7 @@
+﻿public static class StringExtensions
+{
+    public static bool StartsWithUpperCase(this string str)
+    {
+        return !string.IsNullOrEmpty(str) && char.IsUpper(str[0]);
+    }
+}
